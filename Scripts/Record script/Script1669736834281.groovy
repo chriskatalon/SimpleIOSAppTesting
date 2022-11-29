95fd9a176240fb1dbc3ae906171704f5c8b7c625
quan.trinh <quan.trinh@192.168.1.107>
2022-11-29 22:54:18 +0700

    bug dung ko??

--- Scripts/Record script/Script1669736834281.groovy
+++ Scripts/Record script/Script1669736834281.groovy
@@ -17,7 +17,7 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-Mobile.startApplication('kobiton-store:474163', true)
+Mobile.startApplication('', true)
 
 Mobile.tap(findTestObject('Object Repository/XCUIElementTypeOther - myatt_accept_pagebutton_touchableopacity'), 0)
 
